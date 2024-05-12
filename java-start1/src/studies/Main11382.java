b54c7a88c1e703799661840faef7147fb39532fa
taejunkim04 <xowns0622@gmail.com>
2024-05-12 23:53:02 +0900

    plus_11382

---
+++ java-start1/src/studies/Main11382.java
@@ -0,0 +1,13 @@
+package studies;
+
+import java.util.Scanner;
+
+public class Main11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a= scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        System.out.print(a+b+c);
+    }
+}
