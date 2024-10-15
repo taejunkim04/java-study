39fbeab87e976a3961040d2f74ab4cb5579d8248
taejunkim04 <xowns0622@gmail.com>
2024-10-15 22:59:39 +0900

    add_modifyPickList_deletePickList

--- javaexam/src/MidExam.java
+++ javaexam/src/MidExam.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class MidExam {
     //컴퓨터공학과
     //편의점 pos 기기 구현
-    //현재 구현 기능: 상품세팅,상품 보기,
-    //구현 예정 기능: 담은 상품 삭제,담은 내역 확인, (현금 결제시)거스름 지폐계산,
+    //현재 구현 기능: 상품세팅,상품 보기,담은 내역 확인,
+    //구현 예정 기능: 담은 상품 삭제, (현금 결제시)거스름 지폐계산,
     // 통합계산,멤버십 할인 및 기프티콘,결제(결제 실패까지), 영수증 재발행(로그화),환불,현재 매출확인,
     // 상품명 및 가격 변경(권한 설정 및 암호 입력),
     Scanner scanner = new Scanner(System.in);
+    boolean endButton = false;
+    String masterKeyValue = "4356";//관리자 권한 값(가상구현이므로 제시)
+
     int listCount = 20;
     String[] pickListName = new String[listCount];
     int[][] pickListPrice = new int[listCount][2];//가격 갯수
@@ -16,30 +19,39 @@ public class MidExam {
     int productCount = 30;
     String[] productList = new String[productCount];
     int[] productPrice = new int[productCount];
+
+    String[] PayLog = new String[10];
+
     //메인 실행 메서드
     public void run() {
         setProductList();
         while (true) {
             System.out.println("------------------------------------------------------------------------");
             System.out.println("1. 상품 목록 보기 2. 상품 선택 3. 담은 내역 확인 및 수정 4. 결제 5. 직전 영수증 재발행");
-            System.out.println("6. 환불 및 반품 7. 금일 매출 조회 8. 상품명 밎 가격 변경(추가 권한 필요)");
+            System.out.println("관리자 권한 필요: 6. 환불 및 반품 7. 금일 매출 조회 8. 판매 상품명 밎 가격 변경 9. 종료");
             System.out.print("수행을 원하는 작업의 번호를 입력하세요: ");
             String choice = scanner.nextLine();
             switch (choice) {
+                //4,5,6,7,8 미구현
                 case "1" -> showProductList();
                 case "2"-> chooseProducts();
-                case "3"-> showPickList();
-                case "4"->showProductList();
-                case "5"->showProductList();
-                case "6"->showProductList();
-                case "7"->showProductList();
-                case "8"->showProductList();
+                case "3"-> showAndModifyPickList();
+                case "4"-> pickListPay();
+                case "5"-> printLastReceipt();
+                case "6"-> refundAndReturn();
+                case "7"-> getTodaySales();
+                case "8"-> updateProductNameAndPrice();
+                case "9"-> end();
                 default -> System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+            }
+            if (endButton) {
+                System.out.print("서비스를 종료합니다.");
+                return;
             }
         }
     }
 
-    // 메뉴 세팅 메서드
+    // 메뉴 세팅 메서드(임의로 상품 리스트 생성)
     private void setProductList() {
         String[] brandName={"해태","오리온","롯데","CJ","농심"};
         String[] productType={"탄산","주스","과자","조미료","냉동","신선"};
@@ -59,6 +71,7 @@ public class MidExam {
     }
 
     // 1번 옵션 상품 종류 보기 메서드
+    // 한 줄에 5개씩 상품 출력
     private void showProductList() {
         for (int i = 0; i < productCount; i++) {
             System.out.printf("%d. %s(%d) ", i + 1, productList[i],productPrice[i]);
@@ -69,6 +82,12 @@ public class MidExam {
         System.out.println();
     }
     // 2번 옵션 상품 담기
+    // 1. 명령 입력받기
+    // 2. 담기 종료 명령인지 확인
+    // 3. 입력값이 유효한지 타당성 검사, 타당하지 않다면 1로
+    // 4. 앞에서부터 빈공간을 찾아 제품 등록, 빈 공간이 없다면 메뉴로 이동
+    // 5. 이미 등록된 제품이면 기존 라인에 갯수만 추가, 신규면 새로 추가
+    // 6. 입력값이 동시에 여러개라면 4로
     private void chooseProducts() {
         while (true) {
             System.out.println("원하시는 제품의 번호와 수량을 띄어쓰기로 구분하여 입력해주세요.(담기 중단을 원하면 q를 입력하세요.)");
@@ -76,7 +95,7 @@ public class MidExam {
             String wantBye = scanner.nextLine();
             if (wantBye.equals("q") || wantBye.equals("Q")) {
                 System.out.println("담기를 종료하고 메뉴로 돌아갑니다.");
-                break;
+                return;
             }
             if (wantBye.charAt(0) == ' ') {
                 System.out.println("첫 시작이 공백으로 잘못된 입력입니다.");
@@ -98,7 +117,7 @@ public class MidExam {
 
                 if (isNumber) {
                     // 숫자로 변환하여 배열에 추가
-                    if (Integer.parseInt(s) >= productCount) {
+                    if (Integer.parseInt(s) > productCount) {
                         re = true;
                         System.out.println("상품 범위를 초과하거나 비정상적인 값이 있습니다. 다시 입력해주세요.");
                         break;
@@ -149,19 +168,130 @@ public class MidExam {
         }
     }
 
-    // 3번 옵션 선택 메서드
+    // 3번 옵션 담은 내역 확인 및 수정 메서드
+    private void showAndModifyPickList() {
+        while (true) {
+            System.out.println("1. 담은 상품 확인, 2. 담은 상품 수정, 3. 메뉴로 이동");
+            System.out.print("몇 번 명령을 수행할까요?");
+            String choice = scanner.nextLine();
+            if (choice.equals("1")) {
+                showPickList();
+            } else if (choice.equals("2")) {
+                modifyPickList();
+                break;
+            } else if (choice.equals("3")) {
+                return;
+            } else {
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+            }
+        }
+    }
+    //3_1 담은 상품 출력
     private void showPickList() {
         System.out.println("==== 현재 담긴 목록 ====");
         int totalPrice = 0;
+        int totalCount = 0;
         for (int i = 0; i < listCount; i++) {
             if (useListLine[i]) {
                 System.out.printf("%-7s %6d원 %3d개\n", pickListName[i], pickListPrice[i][0], pickListPrice[i][1]);
                 totalPrice += pickListPrice[i][0] * pickListPrice[i][1];
+                totalCount += pickListPrice[i][1];
             }
         }
-        System.out.printf("\t\t합계: %8d원\n", totalPrice);
+        System.out.printf("\t\t 수량: %8d개\n", totalCount);
+        System.out.printf("\t\t 합계: %8d원\n", totalPrice);
     }
-    private void optionSelect(String[] options) {
+    //3_2 담은 상품 수정
+    private void modifyPickList() {
+        while (true) {
+            System.out.println("수정하고자 하는 상품명과 변경할 개수만을 띄어쓰기로 구분하여 입력하세요.(취소는 q를 입력하세요.)");
+            System.out.println("하나씩만 수정할 수 있습니다.,  입력 예시) 삼겹살 5");
+            String choice = scanner.nextLine();
+            if (choice.equals("q") || choice.equals("Q")) {
+                System.out.println("메뉴로 돌아갑니다.");
+                return;
+            }
+            String[] nameAndCount = choice.split(" ");
+            boolean error = false;
+            for (int i = 0; i < nameAndCount[1].length(); i++) {
+                if (!Character.isDigit(nameAndCount[1].charAt(i))) {
+                    // 갯수에 문자가 있으면 재입력
+                    System.out.println("잘못된 입력입니다. 다시 입력해주세요. 입력 예시) 삼겹살 5)");
+                    error = true;
+                    break;
+                }
+            }
+            if (error) {
+                continue;
+            }
+            int changeCount = Integer.parseInt(nameAndCount[1]);
+            boolean found = false;
+            for (int i = 0; i < listCount; i++) {
+                if (useListLine[i]) {
+                    if (pickListName[i].equals(nameAndCount[0])) {
+                        if (changeCount == 0) {
+                            useListLine[i] = false;
+                            found = true;
+                            System.out.println("상품이 목록에서 삭제 되었습니다.");
+                            break;
+                        } else {
+                            pickListPrice[i][1] = changeCount;
+                            found = true;
+                            System.out.println("개수가 변경되었습니다.");
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!found) {
+                System.out.println("해당되는 상품이 없습니다.");
+                continue;
+            }
+            while (true) {
+                System.out.println("1. 계속 변경, 2. 목록 확인 3. 메뉴로 이동");
+                System.out.print("몇 번 명령을 수행할까요?");
+                String again = scanner.nextLine();
+                if (again.equals("1")) {
+                    break;
+                } else if (again.equals("2")) {
+                    showPickList();
+                } else if (again.equals("3")) {
+                    return;
+                } else {
+                    System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+                }
+            }
+        }
+    }
+    // 4번 옵션 결제 메서드
+    private void pickListPay() {
+    }
+    // 5번 옵션 직전 영수증 발행 메서드
+    private void printLastReceipt() {
+    }
+    // 6번 옵션 결제 메서드
+    private void refundAndReturn() {
+    }// 7번 옵션 결제 메서드
+    private void getTodaySales() {
+    }
+    // 8번 옵션 결제 메서드
+    private void updateProductNameAndPrice() {
+    }
+
+
+
+
+    // 9번 옵션 종료 메서드
+    private void end() {
+        System.out.println("종료를 위해 관리자 권한이 있는지 확인합니다.");
+        System.out.printf("암호를 입력해주세요.(암호는 %s 입니다.)", masterKeyValue);
+        String inputPass = scanner.nextLine();
+        if (inputPass.equals(masterKeyValue)) {
+            System.out.println("관리자 권한이 확인되었습니다.");
+            endButton = true;
+        } else {
+            System.out.println("암호가 일치하지 않습니다. 메뉴로 돌아갑니다.");
+        }
     }
 
     // 주문내역 확인
