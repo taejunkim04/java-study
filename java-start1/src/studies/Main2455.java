ec4e72f6bd7dc94123d8eab96bc57d60bff8cb74
taejunkim04 <xowns0622@gmail.com>
2024-10-26 23:26:13 +0900

    20241026study

--- java-start1/src/studies/Main2455.java
+++ java-start1/src/studies/Main2455.java
@@ -3,25 +3,35 @@ package studies;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main2455 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int inSubway = 0;
-        int big = 0;
+        int caseCount = Integer.parseInt(reader.readLine());
         StringTokenizer tk;
-        for (int i = 0; i < 4; i++) {
-            tk = new StringTokenizer(reader.readLine());
-            int down = Integer.parseInt(tk.nextToken());
-            int up = Integer.parseInt(tk.nextToken());
-            inSubway -= down;
-            inSubway += up;
-            if (inSubway > big) {
-                big = inSubway;
+        for (int i = 0; i < caseCount; i++) {
+            int clo = Integer.parseInt(reader.readLine());
+            HashMap<String, Integer> typeList = new HashMap<>();
+            for (int j = 0; j < clo; j++) {
+                tk = new StringTokenizer(reader.readLine());
+                tk.nextToken();
+                String kind = tk.nextToken();
+                if (typeList.containsKey(kind)) {
+                    typeList.put(kind, typeList.get(kind) + 1);
+                } else {
+                    typeList.put(kind, 1);
+                }
             }
+            int result = 1;
+            for (Integer value : typeList.values()) {
+                result *= (value + 1);
+            }
+            result--;
+            System.out.println(result);
+
         }
-        System.out.println(big);
     }
 }
