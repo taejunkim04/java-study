249f8301eedabd5ed7d1d9ab5782b2e25d443784
taejunkim04 <xowns0622@gmail.com>
2025-03-15 23:55:44 +0900

    20250315modify

--- java-start1/src/studies/Main2644.java
+++ java-start1/src/studies/Main2644.java
@@ -7,8 +7,9 @@ import java.util.StringTokenizer;
 
 public class Main2644 {
     //촌수계산
-    static int perCou,startNum, endNum, nodeCou;
+    static int perCou,startNum, endNum, nodeCou, count;
     static boolean[][] map;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         perCou = Integer.parseInt(reader.readLine());
@@ -17,10 +18,44 @@ public class Main2644 {
         endNum = Integer.parseInt(tk.nextToken());
         nodeCou = Integer.parseInt(reader.readLine());
         map = new boolean[perCou + 1][perCou + 1];
+        visited = new boolean[perCou + 1];
         for (int i = 0; i < nodeCou; i++) {
             tk = new StringTokenizer(reader.readLine());
-            map[Integer.parseInt(tk.nextToken())][Integer.parseInt(tk.nextToken())] = true;
+            int fir = Integer.parseInt(tk.nextToken());
+            int sec = Integer.parseInt(tk.nextToken());
+            map[fir][sec] = true;
+            map[sec][fir] = true;
         }
-        
+        count = 1000000;
+        for (int i = 1; i <= perCou; i++) {
+            if (map[startNum][i]) {
+                visited[i] = true;
+                dfs(1, i);
+                visited[i] = false;
+            }
+        }
+
+        if (count == 1000000) {
+            System.out.println(-1);
+        } else {
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int runCou, int curNum) {
+        if (runCou >= count) {
+            return;
+        }
+        if (curNum == endNum) {
+            count = runCou;
+            return;
+        }
+        for (int i = 1; i <= perCou; i++) {
+            if (map[curNum][i] && !visited[i]) {
+                visited[i] = true;
+                dfs(runCou + 1, i);
+                visited[i] = false;
+            }
+        }
     }
 }
